48b64ab62afbdd56414c4122e1241be5922e0303
yansimple <yandanil2017@yandex.ru>
2019-09-18 04:17:30 +0300

    fist commit

---
+++ path.py
@@ -0,0 +1,20 @@
+def prettyPATH(x):
+    items = x.split("/")
+    
+    path_items = list(items[1:])
+
+    for i in path_items:
+        if i == "..":
+            index = path_items.index(i)
+            path_items.pop(index-1)
+            path_items.pop(index-1)
+
+    for i in path_items:
+        if i == ".":
+            index = path_items.index(i)
+            path_items.pop(index)
+
+    string = "/".join(path_items)
+    string = '/'+string
+
+    return string
---
+++ test.py
@@ -0,0 +1,45 @@
+from path import prettyPATH
+
+enter1 = "/var/test/../lib/./foo"
+exit1 = "/var/lib/foo"
+
+enter2 = "/home/.././some/rev/foo"
+exit2 = '/some/rev/foo'
+
+enter3 = '../some/sh/it/.'
+exit3 = '/some/sh/it'
+
+enter4 = '/var/../test/sap/../lib/./foo'
+exit4 = '/lib/foo'
+
+enter5 = '/user/./date/../root/man/foo'
+exit5 = '/user/root/man/foo'
+
+if prettyPATH(enter1) == exit1:
+    print("test 1 = right!")
+else:
+    print("test 1 = wrong!")
+
+
+if prettyPATH(enter1) == exit1:
+    print("test 2 = right!")
+else:
+    print("test 2 = wrong!")
+
+
+if prettyPATH(enter3) == exit3:
+    print("test 3 = right!")
+else:
+    print("test 3 = wrong!")
+
+
+if prettyPATH(enter4) == exit4:
+    print("test 4 = right!")
+else:
+    print("test 4 = wrong!")
+
+
+if prettyPATH(enter5) == exit5:
+    print("test 5 = right!")
+else:
+    print("test 5 = wrong!")
